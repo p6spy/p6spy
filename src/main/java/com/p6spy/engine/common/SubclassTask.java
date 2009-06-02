4d74063ba5ebb02488a7ecb58b6653782f3759fb
Patrick Moore <patmoore@amplafi.com>
2009-06-02 17:16:41 +0000

    code clean up

--- src/main/java/com/p6spy/engine/common/SubclassTask.java
+++ src/main/java/com/p6spy/engine/common/SubclassTask.java
@@ -79,8 +79,6 @@
 package com.p6spy.engine.common;
 
 import javax.sql.DataSource;
-import com.p6spy.engine.spy.P6DataSource;
-
 import java.lang.reflect.Modifier;
 
 import java.io.File;
@@ -89,186 +87,180 @@ import java.io.IOException;
 import java.util.*;
 import java.util.jar.*;
 
-import org.apache.tools.ant.*;
-import org.apache.tools.ant.taskdefs.*;
-import org.apache.tools.ant.types.*;
+import org.apache.tools.ant.AntClassLoader;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.DirectoryScanner;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.FileSet;
+import org.apache.tools.ant.types.Path;
 
 public class SubclassTask extends Task {
 
-  protected String outputDir;
-  protected String outputPackage;
-  protected File baseDir;
+    protected String outputDir;
 
-  protected Path classpath;
-  protected ArrayList fileSets;
-  protected AntClassLoader loader;
+    protected String outputPackage;
 
-  public SubclassTask() {
-    fileSets = new ArrayList();
-  } 
+    protected File baseDir;
 
+    protected Path classpath;
 
-  public void execute() throws BuildException {
-    // use the fileset defined in the parent (abstract) class
-    loader = createClassLoader();
+    protected List<FileSet> fileSets;
 
-    for (Iterator it = fileSets.iterator(); it.hasNext(); ) {
-      FileSet fileset = (FileSet) it.next();
-      DirectoryScanner ds = fileset.getDirectoryScanner(getProject()); 
-      String[] files = ds.getIncludedFiles();
+    protected AntClassLoader loader;
 
-      int size = files.length;
-      for (int i = 0; i < size; i++) {
-	String fileName = files[i];
-
-	if (fileName.endsWith(".class")) {
-	  fixAndLoad(fileName);
-	} else if (fileName.endsWith(".jar")) {
-	  expandJar(ds.getBasedir(), fileName);
-	} else {
-	  log("File '" + fileName + "' is neither a .class nor a .jar file, skipping.",
-	      Project.MSG_WARN);
-	}
-      }
-      
-      //System.out.println(files[i]);
-    }
-  }
-
-  public void fixAndLoad(String fileName) {
-    // strip off the ".class" at the end
-    String cleanString = fileName.substring(0, fileName.indexOf(".class"));
-    StringBuffer className = new StringBuffer();
-
-
-    // change all of the delims to dots... don't use the
-    // replace methods on String to preserve some backwards compat
-    int start = 0;
-    int pos   = 0;
-    while ((pos = cleanString.indexOf(Subclasser.DELIMITER, start)) > 0) {
-      className.append(cleanString.substring(start, pos));
-      className.append(".");
-      start = pos + 1; 
+    public SubclassTask() {
+        fileSets = new ArrayList<FileSet>();
     }
 
-    // catch the tail
-    if (start < cleanString.length()) {
-      className.append(cleanString.substring(start));
+    @Override
+    public void execute() throws BuildException {
+        // use the fileset defined in the parent (abstract) class
+        loader = createClassLoader();
+
+        for (FileSet fileset : fileSets) {
+            DirectoryScanner ds = fileset.getDirectoryScanner(getProject());
+            String[] files = ds.getIncludedFiles();
+
+            if ( files != null) {
+                for (String fileName: files) {
+                    if (fileName.endsWith(".class")) {
+                        fixAndLoad(fileName);
+                    } else if (fileName.endsWith(".jar")) {
+                        expandJar(ds.getBasedir(), fileName);
+                    } else {
+                        log("File '" + fileName + "' is neither a .class nor a .jar file, skipping.", Project.MSG_WARN);
+                    }
+                }
+            }
+        }
+    }
+    public void fixAndLoad(String fileName) {
+        // strip off the ".class" at the end
+        String cleanString = fileName.substring(0, fileName.indexOf(".class"));
+        StringBuffer className = new StringBuffer();
+
+        // change all of the delims to dots... don't use the
+        // replace methods on String to preserve some backwards compat
+        int start = 0;
+        int pos = 0;
+        while ((pos = cleanString.indexOf(Subclasser.DELIMITER, start)) > 0) {
+            className.append(cleanString.substring(start, pos));
+            className.append(".");
+            start = pos + 1;
+        }
+
+        // catch the tail
+        if (start < cleanString.length()) {
+            className.append(cleanString.substring(start));
+        }
+
+        cleanString = className.toString();
+        // try not to further subclass P6 Stuff
+        // this is a little weak, but whatever
+        if (cleanString.indexOf(".P6") == -1) {
+            loadClass(cleanString);
+        }
     }
 
-    cleanString = className.toString();
-    // try not to further subclass P6 Stuff
-    // this is a little weak, but whatever
-    if (cleanString.indexOf(".P6") == -1) {
-      loadClass(cleanString);
-    }
-  }
+    public void loadClass(String className) {
+        try {
+            Class k = loader.loadClass(className);
+            int mods = k.getModifiers();
 
-  public void loadClass(String className) {
-    try {
-      Class k = loader.loadClass(className);
-      int mods = k.getModifiers();
+            // make sure it's a DataSource and something
+            // instantiatable (whew)
+            if (DataSource.class.isAssignableFrom(k) && ((mods & Modifier.ABSTRACT) == 0) && ((mods & Modifier.INTERFACE) == 0)
+                && ((mods & Modifier.FINAL) == 0)) {
 
-      // make sure it's a DataSource and something
-      // instantiatable (whew)
-      if ( 
-	  DataSource.class.isAssignableFrom(k) && 
-	  ((mods & Modifier.ABSTRACT)  == 0) &&
-	  ((mods & Modifier.INTERFACE) == 0) &&
-	  ((mods & Modifier.FINAL)     == 0) 
-	  ) {
+                log("Found class " + className, Project.MSG_WARN);
 
-	log("Found class " + className, Project.MSG_WARN);
+                Subclasser sub = new Subclasser(k);
 
-	Subclasser sub = new Subclasser(k);
+                if (outputPackage != null) {
+                    sub.setOutputPackage(outputPackage);
+                }
 
-	if (outputPackage != null) {
-	  sub.setOutputPackage(outputPackage);
-	}
+                if (outputDir != null) {
+                    sub.setOutputDir(outputDir);
+                }
 
-	if (outputDir != null) {
-	  sub.setOutputDir(outputDir);
-	}
-	
-	sub.createSubClass();
-      }	
-    } catch (Exception e) {
-      throw new BuildException(e);
-    }
-  }
-  
-
-  public void expandJar(File parentDir, String jarName) {
-    try {
-      File jarFS = new File(parentDir, jarName);
-      JarFile jar = new JarFile(jarFS);
-
-      for (Enumeration e = jar.entries(); e.hasMoreElements(); ) {
-	JarEntry entry = (JarEntry) e.nextElement();
-
-	if (!entry.isDirectory() && entry.getName().endsWith(".class")) {
-	  fixAndLoad(entry.getName());
-	}
-
-      }
-    } catch (IOException e) {
-      throw new BuildException(e);
+                sub.createSubClass();
+            }
+        } catch (Exception e) {
+            throw new BuildException(e);
+        }
     }
 
-  }
+    public void expandJar(File parentDir, String jarName) {
+        try {
+            File jarFS = new File(parentDir, jarName);
+            JarFile jar = new JarFile(jarFS);
 
+            for (Enumeration e = jar.entries(); e.hasMoreElements();) {
+                JarEntry entry = (JarEntry) e.nextElement();
 
-  // this is modelled on the code in the Define abstract task
-  protected AntClassLoader createClassLoader() {
-    Path localPath = (classpath == null) ? Path.systemClasspath : classpath;
-    AntClassLoader loader = new AntClassLoader(getProject(), localPath);
-    return loader;
-  }
+                if (!entry.isDirectory() && entry.getName().endsWith(".class")) {
+                    fixAndLoad(entry.getName());
+                }
 
-  // weird lowercaseness for Ant
-  public String getOutputpackage() {
-    return outputPackage;
-  }
+            }
+        } catch (IOException e) {
+            throw new BuildException(e);
+        }
 
-  public void setOutputpackage(String value) {
-    outputPackage= value;
-  }
-
-  public String getOutputdir() {
-    if (outputDir == null) {
-      outputDir = ".";
-    }
-    return outputDir;
-  }
-
-  // do this as a file to let Ant do all the file checking for you
-  public void setOutputdir(File file) {
-    outputDir = file.getAbsolutePath();
-  }
-
-  public void setDir(File dir) {
-    baseDir = dir;
-  }
-
-
-  // these are for the classpath here
-  public void setClasspath(Path cp) {
-    if (classpath == null) {
-      classpath = cp;
-    } else {
-      classpath.append(cp);
-    }
-  }
-
-  public Path createClasspath() {
-    if (classpath == null) {
-      classpath = new Path(getProject());
     }
 
-    return classpath.createPath();
-  }
+    // this is modelled on the code in the Define abstract task
+    protected AntClassLoader createClassLoader() {
+        Path localPath = (classpath == null) ? Path.systemClasspath : classpath;
+        AntClassLoader loader = new AntClassLoader(getProject(), localPath);
+        return loader;
+    }
 
-  public void addFileset(FileSet fs) {
-    fileSets.add(fs);
-  }
+    // weird lowercaseness for Ant
+    public String getOutputpackage() {
+        return outputPackage;
+    }
+
+    public void setOutputpackage(String value) {
+        outputPackage = value;
+    }
+
+    public String getOutputdir() {
+        if (outputDir == null) {
+            outputDir = ".";
+        }
+        return outputDir;
+    }
+
+    // do this as a file to let Ant do all the file checking for you
+    public void setOutputdir(File file) {
+        outputDir = file.getAbsolutePath();
+    }
+
+    public void setDir(File dir) {
+        baseDir = dir;
+    }
+
+    // these are for the classpath here
+    public void setClasspath(Path cp) {
+        if (classpath == null) {
+            classpath = cp;
+        } else {
+            classpath.append(cp);
+        }
+    }
+
+    public Path createClasspath() {
+        if (classpath == null) {
+            classpath = new Path(getProject());
+        }
+
+        return classpath.createPath();
+    }
+
+    public void addFileset(FileSet fs) {
+        fileSets.add(fs);
+    }
 }
