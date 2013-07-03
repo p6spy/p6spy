62641e8f87ed4b0a99250ac81c8a2b9aa1b36b52
Konstantin Burov <aectann@gmail.com>
2013-07-03 22:04:51 +1000

    Removed meaningless test.

--- src/test/java/com/p6spy/engine/spy/P6TestSubclasser.java
+++ src/test/java/com/p6spy/engine/spy/P6TestSubclasser.java
@@ -70,10 +70,7 @@ package com.p6spy.engine.spy;
 
 import static com.p6spy.engine.common.Subclasser.DELIMITER;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 
 import junit.framework.Protectable;
 import junit.framework.TestCase;
@@ -209,67 +206,4 @@ public class P6TestSubclasser extends TestCase {
         }
     }
 
-    public void testClassCreation() throws Exception {
-        Subclasser sub = null;
-
-        createSubclasser(com.p6spy.engine.spy.P6DataSource.class);
-        //    createSubclasser(oracle.jdbc.pool.OracleDataSource.class);
-
-        createSubclasser("com.p6spy.engine.spy.P6DataSource");
-        //    createSubclasser("oracle.jdbc.pool.OracleDataSource");
-    }
-
-    protected void createSubclasser(String name) throws Exception {
-        compareFiles(new Subclasser(name));
-    }
-
-    protected void createSubclasser(Class pc) throws Exception {
-        compareFiles(new Subclasser(pc));
-    }
-
-    protected void compareFiles(Subclasser sub) throws Exception {
-        sub.createSubClass();
-        String name = sub.getOutputName();
-
-        File expected = new File("etc", name + ".java");
-        File actual = sub.getOutputFile();
-
-        compareFiles(expected, actual);
-    }
-
-    protected void compareFiles(File expected, File actual) throws IOException {
-        if (!expected.exists()) {
-            fail("Expected file " + expected + " does not exist.");
-        }
-        if (!actual.exists()) {
-            fail("Output file " + actual + " does not exist.");
-        }
-
-        BufferedReader ebr = new BufferedReader(new FileReader(expected));
-        BufferedReader abr = new BufferedReader(new FileReader(actual));
-        String eline = null;
-        String aline = null;
-        int count = 0;
-        try {
-            while ((eline = ebr.readLine()) != null) {
-                count++;
-                aline = abr.readLine();
-
-                if (aline == null) {
-                    fail(expected.getName() + " " + count + ": No more lines in the output file " + actual.getName());
-                }
-
-                assertEquals("Lines from " + expected.getName() + " and " + actual.getName() + " are not the same", eline, aline);
-            }
-
-            if ((aline = abr.readLine()) != null) {
-                fail(actual.getName() + " " + (count + 1) + ": No more lines in the expected file " + expected.getName());
-            }
-
-        } finally {
-            ebr.close();
-            abr.close();
-        }
-
-    }
 }
