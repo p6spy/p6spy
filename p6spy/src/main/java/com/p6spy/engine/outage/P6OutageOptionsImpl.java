9eb95f4159803ef7498421a280ebfe7c4aeaae43
Peter Butkovic <butkovic@gmail.com>
2013-10-08 01:34:00 +0200

    removed unused classes

--- p6spy/src/main/java/com/p6spy/engine/outage/P6OutageOptionsImpl.java
+++
@@ -1,7 +0,0 @@
-package com.p6spy.engine.outage;
-
-import com.p6spy.engine.common.P6LoadableOptions;
-
-public class P6OutageOptionsImpl implements P6LoadableOptions{
-
-}
--- p6spy/src/main/java/com/p6spy/engine/outage/P6OutageOptionsImplMBean.java
+++
@@ -1,7 +0,0 @@
-package com.p6spy.engine.outage;
-
-public interface P6OutageOptionsImplMBean {
-
-  public boolean isOutageDetection();
-  public long getOutageDetectionInterval();
-}
