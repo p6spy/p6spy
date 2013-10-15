da0972d115dd698869403550256696800a26681b
Peter Butkovic <butkovic@gmail.com>
2013-10-15 23:22:27 +0200

    let's try noninteractive configuration

--- script/travis/firebird.sh
+++ script/travis/firebird.sh
@@ -4,11 +4,8 @@
 sudo apt-get -y install firebird2.5-superclassic
 
 # configure firebird
-sudo dpkg-reconfigure firebird2.5-superclassic -f readline << EOF
-Y
-travis
-EOF
+sudo dpkg-reconfigure firebird2.5-superclassic -fnoninteractive
 
 # create DB in the firebird
-echo "create database /tmp/firebird_p6spy.gdb;" > /tmp/firebird_create_db.sql
+echo "create database '/tmp/firebird_p6spy.gdb';" > /tmp/firebird_create_db.sql
 sudo isql-fb -user sysdba -password travis -i /tmp/firebird_create_db.sql
