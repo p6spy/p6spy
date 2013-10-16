d6fad32c3f845bf2b7dcdbcfff3c46642b19365a
Peter Butkovic <butkovic@gmail.com>
2013-10-16 07:48:43 +0200

    fixed script path + cleanup

--- script/travis/firebird.sh
+++ script/travis/firebird.sh
@@ -4,8 +4,7 @@
 sudo apt-get -y install firebird2.5-superclassic expect
 
 # configure firebird
-#sudo dpkg-reconfigure firebird2.5-superclassic -fnoninteractive
-sudo ./firebird_config.sh
+sudo ./script/travis/firebird_config.sh
 
 # create DB in the firebird
 echo "create database '/tmp/firebird_p6spy.gdb';" > /tmp/firebird_create_db.sql
