9820295311e5481be3b7980834ca2dbb87a2ed24
Peter Butkovic <butkovic@gmail.com>
2013-11-10 23:02:46 +0100

    postgres scriptname fix

--- script/travis/before_script_mysql.sh
+++ script/travis/before_script_mysql.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
 # MySQL
 
-# travis user just to make Vagrant case happy
 mysql -e 'create database p6spy;'
--- script/travis/before_script_postgres.sh
+++ script/travis/before_script_postgres.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
+# PostgreSQL
 
 psql -c 'create database p6spy;' -U postgres
