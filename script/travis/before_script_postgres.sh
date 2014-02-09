59d66ab1f29f7ecf5cd7ed368a711845a484bed3
Peter Butkovic <butkovic@gmail.com>
2014-02-09 15:51:12 +0100

    postgres.conf updates fixed

--- script/travis/before_script_postgres.sh
+++ script/travis/before_script_postgres.sh
@@ -5,7 +5,7 @@ psql -c 'create database p6spy;' -U postgres
 
 pushd /etc/postgresql/*/main/
 # a must for xa transactions
-su -c 'echo "max_prepared_transactions=2" > postgresql.conf'
+sudo sh -c 'echo "max_prepared_transactions=2" >> postgresql.conf'
 echo 'contents of postgresql.conf:'
 cat postgresql.conf
 popd
