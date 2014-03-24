9db1c0a56862abeadf7717084d95a165635f9d54
Peter Butkovic <butkovic@gmail.com>
2014-03-24 21:34:03 +0100

    no need to see postgres.conf contents on each run

--- script/travis/before_script_postgres.sh
+++ script/travis/before_script_postgres.sh
@@ -6,8 +6,8 @@ psql -c 'create database p6spy;' -U postgres
 pushd /etc/postgresql/*/main/
 # a must for xa transactions
 sudo sh -c 'echo "max_prepared_transactions=2" >> postgresql.conf'
-echo 'contents of postgresql.conf:'
-cat postgresql.conf
+#echo 'contents of postgresql.conf:'
+#cat postgresql.conf
 popd
 
 # to apply setting:
