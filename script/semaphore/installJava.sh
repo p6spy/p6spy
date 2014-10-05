7149159bd13126eff4f703580db5a625b20d7027
Quinton McCombs <quinton@mafiabuys.com>
2014-10-04 20:51:24 -0500

    install java

---
+++ script/semaphore/installJava.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Installs groovy
+
+# exist on first failure
+set -e
+
+# download to cache directory if needed
+if [ ! -e $SEMAPHORE_CACHE_DIR/jdk-7u67-linux-x64.tar.gz ] ; then
+  echo "Downloading Java 1.7_67 to $SEMAPHORE_CACHE_DIR"
+  curl --cookie "oraclelicense=accept-securebackup-cookie" \
+     http://download.oracle.com/otn-pub/java/jdk/7u67-b01/jdk-7u67-linux-x64.tar.gz \
+     -o $SEMAPHORE_CACHE_DIR/jdk-7u67-linux-x64.tar.gz
+fi
+
+echo "Installing Java 1.7_67"
+if [ ! -d /home/runner/java ] ; then
+  mkdir -p /home/runner/java
+fi   
+tar -xvzf $SEMAPHORE_CACHE_DIR/jdk-7u67-linux-x64.tar.gz -C /home/runner/java
+
+ls -la /home/runner/java
+
