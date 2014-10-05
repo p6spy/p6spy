0a087b0470f9b8fe6f4bd4c4a6ebe3013950fb6a
Quinton McCombs <quinton@mafiabuys.com>
2014-10-04 21:20:11 -0500

    install java

--- script/semaphore/installOracleJava6.sh
+++ script/semaphore/installOracleJava6.sh
@@ -7,8 +7,8 @@ set -e
 # download to cache directory if needed
 if [ ! -e $SEMAPHORE_CACHE_DIR/jdk-6u45-linux-x64.bin ] ; then
   echo "Downloading Java 1.6u45 to $SEMAPHORE_CACHE_DIR"
-  curl -L  \
-     http://download.oracle.com/otn/java/jdk/6u45-b06/jdk-6u45-linux-x64.bin?AuthParam=1412474638_4e2e9710258a88902ed80c56340ab6b3 \
+  curl -L --cookie "oraclelicense=accept-securebackup-cookie" \
+     http://download.oracle.com/otn-pub/java/jdk/6u45-b06/jdk-6u45-linux-x64.bin \
      -o $SEMAPHORE_CACHE_DIR/jdk-6u45-linux-x64.bin
 fi
 
