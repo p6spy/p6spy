779187c381b5339b0aa281c1fa3fdee4b6dcbd79
Quinton McCombs <quinton.mccombs@gmail.com>
2014-10-08 13:43:20 -0500

    #264 - add support for logging resultset column values when resultset is closed.

--- src/main/java/com/p6spy/engine/logging/P6LogResultSetCloseDelegate.java
+++ src/main/java/com/p6spy/engine/logging/P6LogResultSetCloseDelegate.java
@@ -19,7 +19,6 @@
  */
 package com.p6spy.engine.logging;
 
-import com.p6spy.engine.common.P6LogQuery;
 import com.p6spy.engine.common.ResultSetInformation;
 import com.p6spy.engine.proxy.Delegate;
 
@@ -29,10 +28,10 @@ import java.lang.reflect.Method;
  * @author Quinton McCombs
  * @since 09/2013
  */
-class P6LogResultSetNextDelegate implements Delegate {
+class P6LogResultSetCloseDelegate implements Delegate {
   private final ResultSetInformation resultSetInformation;
 
-  public P6LogResultSetNextDelegate(final ResultSetInformation resultSetInformation) {
+  public P6LogResultSetCloseDelegate(final ResultSetInformation resultSetInformation) {
     this.resultSetInformation = resultSetInformation;
   }
 
@@ -49,32 +48,14 @@ class P6LogResultSetNextDelegate implements Delegate {
    */
   @Override
   public Object invoke(final Object proxy, final Object underlying, final Method method, final Object[] args) throws Throwable {
-    long startTime = System.currentTimeMillis();
-    Object result = null;
-    try {
       
-      if (resultSetInformation.getCurrRow() > -1) {
-        /*
-           Log the columns that were accessed except on the first call to ResultSet.next().  The first time it is
-           called it is advancing to the first row.  
-          */
-        resultSetInformation.generateLogMessage();
-      }
-      
-      result = method.invoke(underlying, args);
-      
-      if( Boolean.TRUE.equals(result) ) {
-        // only increment the current row number if the result set advanced to the next row
-        resultSetInformation.setCurrRow(resultSetInformation.getCurrRow() + 1);
-      }
-      
-      return result;
-    } finally {
-      // the result of the proxied method call will be true or false since this is used to proxy the call to ResultSet.next()
-      // we do not need to log the call if the result was false as it means that there were no more results.
-      if( Boolean.TRUE.equals(result) ) {
-        P6LogQuery.logElapsed(resultSetInformation.getConnectionId(), startTime, Category.RESULT, resultSetInformation);
-      }
+    if ( resultSetInformation.getCurrRow() > -1 ) {
+      //  If the result set has not been advanced to the first row, there is nothing to log.
+      resultSetInformation.generateLogMessage();
     }
+    
+    return method.invoke(underlying, args);
+    
+    // Note: No timing is logged on calls to ResultSet.close()
   }
 }
