3639f995b3f531cd266d62e55504a2631b4bca54
Quinton McCombs <quinton.mccombs@gmail.com>
2015-05-20 15:27:22 -0500

    lazy initialization for FileLogger
    fixes #290

--- src/main/java/com/p6spy/engine/spy/appender/StdoutLogger.java
+++ src/main/java/com/p6spy/engine/spy/appender/StdoutLogger.java
@@ -19,31 +19,30 @@
  */
 package com.p6spy.engine.spy.appender;
 
-import java.io.PrintStream;
-
 import com.p6spy.engine.logging.Category;
 
+import java.io.PrintStream;
+
 public class StdoutLogger extends FormattedLogger {
-    protected PrintStream qlog;
 
-    public StdoutLogger() {
-      qlog = System.out;
-    }
-    
-    @Override
-    public void logException(Exception e) {
-      e.printStackTrace(qlog);
-    }
+  protected PrintStream getStream() {
+    return System.out;
+  }
 
-    @Override
-    public void logText(String text) {
-      qlog.println(text);
-    }
+  @Override
+  public void logException(Exception e) {
+    e.printStackTrace(getStream());
+  }
 
-        @Override
-        public boolean isCategoryEnabled(Category category) {
-                // no restrictions on logger side
-                return true;
-        }
+  @Override
+  public void logText(String text) {
+    getStream().println(text);
+  }
+
+  @Override
+  public boolean isCategoryEnabled(Category category) {
+    // no restrictions on logger side
+    return true;
+  }
 }
 
