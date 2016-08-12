3075397331a4c948ef60013c58029be9bfb7a822
Felix Barnsteiner <felix.barnsteiner@isys-software.de>
2016-08-12 11:18:05 +0200

    Try to fix firebird test on travis

--- script/travis/before_script_firebird.sh
+++ script/travis/before_script_firebird.sh
@@ -2,6 +2,6 @@
 # Firebird
 
 isql-fb -z -q -i /dev/null # --version
-echo "create database '/tmp/firebird_p6spy.gdb';" > /tmp/firebird_create_db.sql # preps for test DB creation
+echo "create database '127.0.0.1:/tmp/firebird_p6spy.gdb';" > /tmp/firebird_create_db.sql # preps for test DB creation
 sudo isql-fb -user sysdba -password masterkey -i /tmp/firebird_create_db.sql # test DB creation
 
