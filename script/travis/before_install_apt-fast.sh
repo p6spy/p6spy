132466d7fbe2305edb5689b89124b8ade6f71b19
Peter Butkovic <butkovic@gmail.com>
2017-07-08 19:39:03 +0200

    chore: apt-fast install fixed for trusty

--- script/travis/before_install_apt-fast.sh
+++ script/travis/before_install_apt-fast.sh
@@ -2,7 +2,8 @@
 # Installs apt-fast to speed up package downloads
 #
 # http://www.webupd8.org/2012/10/speed-up-apt-get-downloads-with-apt.html
+# https://github.com/ilikenwf/apt-fast
 
-sudo add-apt-repository -y ppa:apt-fast/stable
+sudo add-apt-repository -y ppa:saiarcot895/myppa
 sudo apt-get update -qq -y
 sudo apt-get install -qq -y --force-yes apt-fast
