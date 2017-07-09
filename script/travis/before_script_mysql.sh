6bd28aa5768ca9dee34ffe7245c9b429d69f687f
Peter Butkovic <butkovic@gmail.com>
2017-07-10 00:27:09 +0200

    chore: MySQL grant  p6spy DB for travis user

--- script/travis/before_script_mysql.sh
+++ script/travis/before_script_mysql.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
 # MySQL
-
 mysql -e 'create database p6spy;'
+
+# otherwise error on trusty: com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException: Access denied for user 'travis'@'%' to database 'p6spy'
+mysql -e "GRANT ALL ON p6spy.* TO 'travis'@'%';"
+mysql -e "GRANT ALL ON p6spy TO 'travis'@'%';"
+mysql -e "GRANT CREATE ON p6spy TO 'travis'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
