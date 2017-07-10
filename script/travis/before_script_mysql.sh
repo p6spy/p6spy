b07de51400598d116af4b1fcda3e6786ef6e2cdb
Peter Butkovic <butkovic@gmail.com>
2017-07-10 06:45:36 +0200

    chore: MySQL uses root user

--- script/travis/before_script_mysql.sh
+++ script/travis/before_script_mysql.sh
@@ -2,10 +2,3 @@
 # MySQL
 
 mysql -e "create database p6spy;"
-
-# mysql -e "\
-# create database p6spy; \
-# GRANT ALL ON p6spy.* TO 'travis'@'%'; \
-# GRANT ALL ON p6spy TO 'travis'@'%'; \
-# GRANT CREATE ON p6spy TO 'travis'@'%'; \
-# FLUSH PRIVILEGES;"
