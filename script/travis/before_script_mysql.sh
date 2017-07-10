44be271b1628015f53b826d2d58f81c2fae37a04
Peter Butkovic <butkovic@gmail.com>
2017-07-10 06:36:31 +0200

    chore: MySQL no grant

--- script/travis/before_script_mysql.sh
+++ script/travis/before_script_mysql.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 # MySQL
 
-mysql -e "\
-create database p6spy; \
-GRANT ALL ON p6spy.* TO 'travis'@'%'; \
-GRANT ALL ON p6spy TO 'travis'@'%'; \
-GRANT CREATE ON p6spy TO 'travis'@'%'; \
-FLUSH PRIVILEGES;"
+mysql -e "create database p6spy;"
+
+# mysql -e "\
+# create database p6spy; \
+# GRANT ALL ON p6spy.* TO 'travis'@'%'; \
+# GRANT ALL ON p6spy TO 'travis'@'%'; \
+# GRANT CREATE ON p6spy TO 'travis'@'%'; \
+# FLUSH PRIVILEGES;"
