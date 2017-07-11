7ffe76d4da73e550d2f953da1207b0aa44023426
Peter Butkovic <butkovic@gmail.com>
2017-07-11 09:41:14 +0200

    feat: coveralls + bintray publishing

--- script/travis/before_script_firebird.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-# Firebird
-
-isql-fb -z -q -i /dev/null # --version
-echo "create database '/tmp/firebird_p6spy.gdb';" > /tmp/firebird_create_db.sql # preps for test DB creation
-sudo isql-fb -user sysdba -password masterkey -i /tmp/firebird_create_db.sql # test DB creation
-
