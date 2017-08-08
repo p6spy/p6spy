4c741fc3d2e67cd5160664c0f62a4eac79efd8af
Peter Butkovic <butkovic@gmail.com>
2017-08-08 23:35:50 +0200

    chore: cleanup pre-docker + pre-gradle leftovers

--- script/travis/before_install_apt-fast.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-# Installs apt-fast to speed up package downloads
-#
-# http://www.webupd8.org/2012/10/speed-up-apt-get-downloads-with-apt.html
-# https://github.com/ilikenwf/apt-fast
-
-sudo add-apt-repository -y ppa:saiarcot895/myppa
-sudo apt-get update -qq -y
-sudo apt-get install -qq -y --force-yes apt-fast
