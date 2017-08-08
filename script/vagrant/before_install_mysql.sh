d5d5b5fef0918ed1181cd3ca2d6f123afd5a9f0b
Peter Butkovic <butkovic@gmail.com>
2017-08-08 23:30:50 +0200

    chore: cleanup vagrant leftovers

--- script/vagrant/before_install_mysql.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-# set root pwd to empty
-mysql -u root -p'123' -e "use mysql; update user set password=PASSWORD('') where User='root'; flush privileges;"
-
-# add user 'travis with empty pwd
-mysql -u root -e "CREATE USER 'travis'@'localhost' IDENTIFIED BY '';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON * . * TO 'travis'@'localhost';"
